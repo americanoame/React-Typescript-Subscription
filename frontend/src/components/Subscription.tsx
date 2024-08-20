43978a2f38bec439fa29eb84f022c4537d03fe75
Americano.Dev <andrenations@gmail.com>
2024-08-20 18:52:02 -0400

    Initialize Subscription component with basic structure

---
+++ frontend/src/components/Subscription.tsx
@@ -0,0 +1,13 @@
+const Subscription: React.FC = () => {
+  return (
+    <div className="subscription-container">
+      <div className="subscription-content">
+        <div style={{ marginLeft: '10px' }}></div>
+
+        <div className="line"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Subscription;
